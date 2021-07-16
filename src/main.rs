a9ebaba7985e3ff962a546adb4001a472cf26761
Arif Driessen <arifd86@gmail.com>
2021-07-16 18:29:24 +0200

    initialize project

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use tide::prelude::*;
+use tide::Request;
+
+#[derive(Debug, Deserialize)]
+struct Animal {
+    name: String,
+    legs: u8,
+}
+
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    let mut app = tide::new();
+    app.at("/orders/shoes").post(order_shoes);
+    app.at("/test").get(test);
+    app.at("/").get(hello);
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
+}
+
+async fn hello(_: Request<()>) -> tide::Result {
+    Ok("hello".into())
+}
+
+async fn test(_: Request<()>) -> tide::Result {
+    Ok("hello fucken world".into())
+}
+
+async fn order_shoes(mut req: Request<()>) -> tide::Result {
+    let Animal { name, legs } = req.body_json().await?;
+    println!("request from: {}", name);
+    Ok(format!("Hello, {}! I've put in an order for {} shoes", name, legs).into())
+}
